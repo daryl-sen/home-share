5cb720789686e6c1fce8029a0a4ed8f689f264ba
Daryl <tsdaryl@gmail.com>
2022-09-20 21:33:06 -0700

    configure app for swagger ui express

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,12 +1,28 @@
 import express from "express";
 import path from "path";
+import swaggerUi from "swagger-ui-express";
+const YAML = require("yamljs");
 
 const app = express();
 const port = 3200;
 
+const swaggerDocument = YAML.load(path.join(__dirname, "../src/api-doc.yml"));
+console.log(swaggerDocument);
+
 const rootPath = path.join(__dirname);
+console.log(rootPath);
 
 app.use(express.static(rootPath));
+
+app.use(
+  "/api-docs",
+  swaggerUi.serve,
+  swaggerUi.setup(swaggerDocument, {
+    swaggerOptions: {
+      validatorUrl: undefined,
+    },
+  })
+);
 
 app.get("/api", (req, res) => {
   res.send("Backend API coming soon!");
