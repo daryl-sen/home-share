a8b4646b6fd98cbacc7dc94184d627e835c07295
Daryl <tsdaryl@gmail.com>
2022-09-24 15:02:59 -0700

    completely remove backend directory

--- backend/src/controllers/hello-world-controller.ts
+++
@@ -1,9 +0,0 @@
-import { Request, Response } from "express";
-
-const helloWorldController = {
-  getHelloWorld: (_req: Request, res: Response) => {
-    res.send("hello world!");
-  },
-};
-
-module.exports = helloWorldController;
