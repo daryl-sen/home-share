c0ea5f4b43881d0d93fbf8ea4940655e42548cad
Daryl Tang <daryl.tang@zayzoon.com>
2023-02-10 11:06:59 -0800

    create virus contact us page

--- src/modules/functional/modules/Contact/ContactIndex.tsx
+++ src/modules/functional/modules/Contact/ContactIndex.tsx
@@ -1,7 +1,16 @@
 import React from 'react';
 
 import PageContainer from '../../../layout/modules/PageContainer';
+import VirusPopup from './VirusPopup';
 
 export default function Todo() {
-  return <PageContainer>Todo</PageContainer>;
+  return <PageContainer>
+    <h1>Contact Us</h1>
+
+    <p>Need help? We might be able to, or maybe not. Call us and let's see.</p>
+
+    <h2>604-NOT-SCAM</h2>
+
+    <VirusPopup />
+  </PageContainer>;
 }
---
+++ src/modules/functional/modules/Contact/VirusPopup.tsx
@@ -0,0 +1,63 @@
+import * as React from 'react';
+
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import LinearProgress from '@mui/material/LinearProgress';
+import Modal from '@mui/material/Modal';
+import Typography from '@mui/material/Typography';
+
+const style = {
+  position: 'absolute' as 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  boxShadow: 24,
+  p: 4,
+};
+
+
+export default function VirusPopup() {
+  const [open, setOpen] = React.useState(false);
+  const [virus, setVirus] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+  
+  function showVirusDetected() {
+    if (virus) {
+      return "We have found a 1,337 viruses on your computer!! Contact us now so your precious files won't be deleted."
+    }
+  }
+
+  React.useEffect(() => {
+    setTimeout(() => {
+      setVirus(true)
+    }, 5000)
+  })
+
+  return (
+    <div>
+      <Button onClick={handleOpen}>Live Chat</Button>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <Typography id="modal-modal-title" variant="h6" component="h2" sx={{ mb: 6 }}>
+            Virus <s>Scam</s> Scan
+          </Typography>
+          <LinearProgress />
+          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+            Please wait while we scan your computer for viruses
+          </Typography>
+          <Typography id="modal-modal-description" sx={{ mt: 2, color: "red" }}>
+            { showVirusDetected() }
+          </Typography>
+        </Box>
+      </Modal>
+    </div>
+  );
+}
