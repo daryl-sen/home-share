6458e0e328b899bf4d47b42d78342fbae909e4f4
Daryl Tang <daryl.tang@zayzoon.com>
2023-02-10 10:31:05 -0800

    create landing page

--- src/modules/functional/modules/Dashboard/DashboardIndex.tsx
+++ src/modules/functional/modules/Dashboard/DashboardIndex.tsx
@@ -1,6 +1,18 @@
-import PageContainer from "../../../layout/modules/PageContainer";
-import React from "react";
+import React from 'react';
+
+import PageContainer from '../../../layout/modules/PageContainer';
 
 export default function Dashboard() {
-  return <PageContainer>Dashboard</PageContainer>;
+  return <PageContainer>
+    <h1>Welcome to LegitBank Canada</h1>
+    <p>We are the most legit bank<sup>1</sup> in all of Canada, maybe North America.</p>
+    <p>Please log in with your real bank credentials from any bank, this is not a scam<sup>2</sup>.</p>
+
+    <img src="https://d2tyltutevw8th.cloudfront.net/media/image/open-banking-1200-1662083126.jpg" alt="" />
+    <p>
+      <sup>1</sup> Not a real bank. <br />
+      <sup>2</sup> This is definitely a scam
+    </p>
+
+  </PageContainer>;
 }
